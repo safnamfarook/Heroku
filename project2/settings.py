649b7bf90d82ae68a90a79174379c680f741e096
Safna Muhammed Farook <sf20aav@herts.ac.uk>
2022-03-31 11:39:56 +0100

    First Commit

--- project2/settings.py
+++ project2/settings.py
@@ -127,4 +127,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = BASE_DIR/"staticfiles"
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
